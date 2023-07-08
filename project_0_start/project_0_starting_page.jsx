6c1791990908bae62df4bdd2434ba8f1bf4f6af8
Sam Ilya <samuel.ilyayev@gmail.com>
2023-07-07 21:28:26 -0400

    added react app

--- project_0_start/project_0_starting_page.jsx
+++
@@ -1,5 +0,0 @@
-const element = <h1>Hello, world!</h1>;
-const element2 = <h1> Avi is top g </h1>;
-const element3 = <h1> wasgood in da hood </h1>;
-const element4 = <h2> hey </h2>;
-const element5 = <h1> wasgood in da hood </h1>; 
